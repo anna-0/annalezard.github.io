fb3b94a3a70b05becf95309fe5beea35401282fd
Anna Lezard <62560017+anna-0@users.noreply.github.com>
2021-02-02 15:20:55 +0000

    Fixed subheading bug

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-        
     const title = document.querySelector('#title');
+    const subheading = document.querySelector('#subheading');
     let stuck = title.offsetTop;
     const copyButton = document.querySelector('#copy');
-    copyButton.addEventListener('click', ()=> copyToClipboard(document.querySelector('#email')));
-    copyButton.addEventListener('mouseleave', ()=> tooltipOut());
+    copyButton.addEventListener('click', () =>
+        copyToClipboard(document.querySelector('#email'))
+    );
+    copyButton.addEventListener('mouseleave', () => tooltipOut());
 
     const arrow = document.querySelector('#scroll');
-    arrow.addEventListener('click', ()=> scrollTo('portfolio'));
-    
+    arrow.addEventListener('click', () => scrollTo('portfolio'));
+
     window.onscroll = () => {
         if (window.pageYOffset >= stuck) {
             title.className = 'stuck';
+            document.querySelector('#subheading').style.display = 'none';
             document.querySelector('#arrow').style.opacity = 0;
-        }
+        } 
         else if (window.pageYOffset < stuck) {
             document.querySelector('#arrow').style.opacity = 1;
+            document.querySelector('#subheading').style.display = 'block';
             title.className = 'start';
         }
-    }
-})
+    };
+});
 
 function copyToClipboard(element) {
-
     window.getSelection().removeAllRanges();
     let range = document.createRange();
     range.selectNode(element);
@@ -35,13 +37,13 @@ function copyToClipboard(element) {
     tooltip.innerHTML = 'Copied!';
     document.querySelector('.clipboard').style.color = '#d6eeed';
     document.querySelector('#copy').style.backgroundColor = '#1a2e34';
-  }
+}
 
 function tooltipOut() {
     document.querySelector('.tooltiptext').innerHTML = 'Copy to clipboard';
 }
 
-function scrollTo(place){
+function scrollTo(place) {
     let url = location.href;
     location.href = '#' + place;
     history.replaceState(null, null, url);
