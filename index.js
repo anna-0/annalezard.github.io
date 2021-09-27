3f95563ceda1ad43923e23b35ad070f12587bc50
anna <anna@eebria.com>
2021-09-28 00:01:09 +0100

    updated with new project

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const title = document.querySelector("#title");
-  const subheading = document.querySelector("#subheading");
+  const avi = document.querySelector(".avi");
   let stuck = title.offsetTop;
   const copyButton = document.querySelector("#copy");
   copyButton.addEventListener("click", () =>
-    copyToClipboard(document.querySelector("#email"))
+    copyToClipboard(document.querySelector("#email").textContent)
   );
   copyButton.addEventListener("mouseleave", () => tooltipOut());
 
@@ -14,36 +14,44 @@ document.addEventListener("DOMContentLoaded", () => {
   window.onscroll = () => {
     if (window.pageYOffset >= stuck) {
       title.className = "stuck";
+      avi.classList.add("stuckImg");
+      avi.parentElement.classList.add("stuckDiv");
       document.querySelector("#subheading").style.display = "none";
       document.querySelector("#arrow").style.opacity = 0;
     } else if (window.pageYOffset < stuck) {
       document.querySelector("#arrow").style.opacity = 1;
       document.querySelector("#subheading").style.display = "block";
+      avi.classList.remove("stuckImg");
+      avi.parentElement.classList.remove("stuckDiv");
       title.className = "start";
     }
   };
 });
 
-function copyToClipboard(element) {
-  window.getSelection().removeAllRanges();
-  let range = document.createRange();
-  range.selectNode(element);
-  window.getSelection().addRange(range);
-  document.execCommand("copy");
-  window.getSelection().removeAllRanges();
+const copyToClipboard = (element) => {
+  if (!navigator.clipboard) {
+    window.getSelection().removeAllRanges();
+    let range = document.createRange();
+    range.selectNode(element);
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+  } else {
+    navigator.clipboard.writeText(element).then(function () {
+      const tooltip = document.querySelector(".tooltiptext");
+      tooltip.innerHTML = "Copied!";
+      document.querySelector(".clipboard").style.color = "#d6eeed";
+      document.querySelector("#copy").style.backgroundColor = "#1a2e34";
+    });
+  }
+};
 
-  const tooltip = document.querySelector(".tooltiptext .copytext");
-  tooltip.innerHTML = "Copied!";
-  document.querySelector(".clipboard").style.color = "#d6eeed";
-  document.querySelector("#copy").style.backgroundColor = "#1a2e34";
-}
-
-function tooltipOut() {
+const tooltipOut = () => {
   document.querySelector(".tooltiptext").innerHTML = "Copy to clipboard";
-}
+};
 
-function scrollTo(place) {
+const scrollTo = (place) => {
   let url = location.href;
   location.href = "#" + place;
   history.replaceState(null, null, url);
-}
+};
